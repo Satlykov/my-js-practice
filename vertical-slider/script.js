44c6a5d689e417772150d18e8a913e20704ac9f5
Rustam Satlykov <satlikov.rustam.mail@gmail.com>
2021-08-20 01:04:22 +0500

    feat: add wheel scroll

--- vertical-slider/script.js
+++ vertical-slider/script.js
@@ -6,6 +6,9 @@ const slidesCount = mainSlide.querySelectorAll('div').length
 const container = document.querySelector('.container')
 
 let activeSlideIndex = 0
+let isDrag = false
+let start
+let end
 
 sidebar.style.top = `-${(slidesCount - 1) * 100}vh`
 
@@ -16,6 +19,28 @@ upBtn.addEventListener('click',() => {
 downBtn.addEventListener('click',() => {
     changeSlide('down')
 })
+
+container.addEventListener('wheel',(event) => {
+    if (event.deltaY > 0) {
+        changeSlide('up')
+    }
+    if (event.deltaY < 0) {
+        changeSlide('down')
+    }
+})
+
+container.addEventListener('mousedown',(event) => {
+    isDrag = true
+    start = event.clientY
+    console.log(start)
+})
+
+container.addEventListener('mouseup',(event) => {
+    isDrag = false
+    end = event.clientY
+    console.log(end)
+})
+
 
 function changeSlide(direction) {
     if(direction === 'up') {
