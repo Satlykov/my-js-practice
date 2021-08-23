832c23040f3bbaa0435ad0470fe7cf0c26b06695
Rustam Satlykov <satlikov.rustam.mail@gmail.com>
2021-08-24 00:47:19 +0500

    feat: add vertical scroll

--- vertical-slider/script.js
+++ vertical-slider/script.js
@@ -1,12 +1,18 @@
 const upBtn = document.querySelector('.up-button')
 const downBtn = document.querySelector('.down-button')
+const changeBtn = document.querySelector('.сhange-button')
+const imgBtnLeft = document.getElementById('left')
+const imgBtnRight = document.getElementById('right')
 const sidebar = document.querySelector('.sidebar')
+const sidebarContainer = document.querySelector('.container-sidebar')
 const mainSlide = document.querySelector('.main-slide')
-const slidesCount = mainSlide.querySelectorAll('div').length
+const mainSlideContainer = document.querySelector('.container-main-slide')
+const slidesCount = mainSlideContainer.querySelectorAll('div').length
 const container = document.querySelector('.container')
 
 let activeSlideIndex = 0
 let isDrag = false
+let vertical = false
 let start
 let end
 
@@ -18,6 +24,20 @@ upBtn.addEventListener('click',() => {
 
 downBtn.addEventListener('click',() => {
     changeSlide('down')
+})
+
+changeBtn.addEventListener('click',() => {
+    imgBtnRight.classList.toggle('fa-arrow-right')
+    imgBtnRight.classList.toggle('fa-arrow-up')
+    imgBtnLeft.classList.toggle('fa-arrow-left')
+    imgBtnLeft.classList.toggle('fa-arrow-down')
+
+    sidebar.classList.toggle('sidebar-vertical')
+    sidebarContainer.classList.toggle('container-sidebar-vertical')
+    mainSlide.classList.toggle('main-slide-vertical')
+    mainSlideContainer.classList.toggle('container-main-slide-vertical')
+
+    vertical === false ? vertical = true : vertical = false
 })
 
 container.addEventListener('wheel',(event) => {
@@ -43,21 +63,43 @@ container.addEventListener('mouseup',(event) => {
 
 
 function changeSlide(direction) {
-    if(direction === 'up') {
-        activeSlideIndex++
-        if (activeSlideIndex === slidesCount) {
-            activeSlideIndex = 0
+    if (!vertical) {
+        if(direction === 'up') {
+            activeSlideIndex++
+            if (activeSlideIndex === slidesCount) {
+                activeSlideIndex = 0
+            }
+        } else if (direction === 'down') {
+            activeSlideIndex--
+            if (activeSlideIndex < 0) {
+                activeSlideIndex = slidesCount - 1
+            }
         }
-    } else if (direction === 'down') {
-        activeSlideIndex--
-        if (activeSlideIndex < 0) {
-            activeSlideIndex = slidesCount - 1
-        }
+    
+        const height = container.clientHeight
+    
+        mainSlideContainer.style.transform = `translateY(-${activeSlideIndex * height}px)`
+    
+        sidebarContainer.style.transform = `translateY(${activeSlideIndex * height}px)`
     }
 
-    const height = container.clientHeight
-
-    mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`
-
-    sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`
+    if (vertical) {
+        if(direction === 'up') {
+            activeSlideIndex++
+            if (activeSlideIndex === slidesCount) {
+                activeSlideIndex = 0
+            }
+        } else if (direction === 'down') {
+            activeSlideIndex--
+            if (activeSlideIndex < 0) {
+                activeSlideIndex = slidesCount - 1
+            }
+        }
+    
+        const width = (container.clientWidth * 0.65)
+    
+        mainSlideContainer.style.transform = `translateX(-${activeSlideIndex * width}px)`
+    
+        sidebarContainer.style.transform = `translateX(${activeSlideIndex * width}px)`
+    }
 }
